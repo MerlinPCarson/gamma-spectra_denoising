11a887bac1f6b6353af00e64f16041fb557bb2ee
Merlin <mpc6@gmail.com>
2021-04-17 15:12:18 -0700

    adding script to plot figures for papers

---
+++ PaperArtifacts.py
@@ -0,0 +1,136 @@
+import os
+import sys
+import time
+import json
+import argparse
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from plot_utils import compare_spectra
+from spectra_utils import split_radionuclide_name
+from spectra_utils import data_smooth
+
+
+def compare_spectra(keV, spectra, titles, min_keV=-10, max_keV=1500, outfile=None, savefigs=False, showfigs=True):
+
+    colors = ['blue', 'red']
+
+    min_idx = np.searchsorted(keV, min_keV, side='left')
+    max_idx = np.searchsorted(keV, max_keV, side='left')
+
+    plt.figure(figsize=(20,10))
+    for spectrum, title, color in zip(spectra, titles, colors):
+        plt.plot(keV[min_idx:max_idx], spectrum[min_idx:max_idx], color=color, label=f'{title}')
+    
+    ax = plt.gca()
+    ax.set_xlabel('Energy (keV)', fontsize=24, fontweight='bold', fontname='cmtt10')
+    ax.set_ylabel('Intensity', fontsize=24, fontweight='bold', fontname='cmtt10')
+    ax.set_xticks(np.arange(keV[min_idx], keV[max_idx], 100))
+    ax.set_xticks(np.arange(keV[min_idx], keV[max_idx], 20), minor=True)
+    ax.set_xlim([keV[min_idx], keV[max_idx]])
+    ax.grid(axis='x', which='major', alpha=0.5)
+    ax.grid(axis='x', which='minor', alpha=0.2)
+
+    plt.legend(fancybox=True, shadow=True, fontsize=24)
+    plt.tight_layout()
+
+    if savefigs and outfile:
+        plt.savefig(outfile, format='pdf', dpi=300)
+
+    if showfigs:
+        plt.show()
+
+    plt.close()
+
+def load_spectrum(spectrum, title, min_keV, max_keV, normalize):
+    
+    spec = json.load(open(spectrum, 'r'))
+    keV = spec['KEV']
+    hits = spec['HIT']
+    try:
+        rn = spec['RADIONUCLIDE']
+    except:
+        rn = None
+
+    if normalize:
+        hits = np.array(hits, dtype=np.float)
+        hits /= np.sum(hits)
+
+    return keV, hits, rn
+
+def show_spectra(keV, hits, rn, titles, min_keV, max_keV, outfile):
+
+    fig_titles = []
+
+    rn_num, rn_name = split_radionuclide_name(rn)
+
+    for title in titles:
+        fig_titles.append("${}^{"+rn_num+"}{"+rn_name+"}$ " + title)
+
+    compare_spectra(keV, hits, fig_titles, min_keV, max_keV, outfile, savefigs=True)
+
+def nai_effeciency(keV, cutoff=121.8, max_eff=1400):
+    if keV <= cutoff:
+        eff = 0.0036*keV + 0.5641
+        return max(eff,0.0)
+    else:
+        eff = 8.9946*(keV**(-0.462))
+        return max(eff,0.0)
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description='Plot multiple spectra')
+    parser.add_argument("-cf", "--configfile", help="configuration file for generating data", default="config_data_real.json")
+    parser.add_argument("-out", "--outdir", help="output directory for figures", default="figures")
+    parser.add_argument("-det", "--dettype", help="detector type", default="NaI")
+    parser.add_argument('--nndc_temp', type=str, default='data/mcnp_spectra/spectra/compton/152Eu_1mc_1s-no-compton.json', 
+                        help='photoelectric peaks for a radionuclide')
+    parser.add_argument('--wide_temp', type=str, default='data/mcnp_spectra/preproc_spectra/152Eu_1mc_1s-nocompton.json', 
+                        help='photoelectric peaks for a radionuclide')
+    parser.add_argument('--min_keV', type=float, default=0.0, help='minimum keV to plot')
+    parser.add_argument('--max_keV', type=float, default=1500.0, help='maximum keV to plot')
+    parser.add_argument('--normalize', default=False, action='store_true', help='normalize all spectra')
+    args = parser.parse_args()
+
+    return args
+
+def main(args):
+    start = time.time()
+
+    # load configuration parameters
+    with open(args.configfile, 'r') as cfile:
+        config = json.load(cfile)['DETECTORS'][args.dettype.upper()]
+
+    # load a radionuclide template
+    keV, hits, rn, = load_spectrum(args.nndc_temp, 'NNDC gamma-ray table', args.min_keV, args.max_keV, True)
+
+    # plot a template 
+    titles = ['NNDC gamma-ray table']
+    outfile = os.path.join(args.outdir, 'template.pdf')
+    show_spectra(keV, [hits], rn, titles, args.min_keV, args.max_keV, outfile)
+
+    # apply efficiency
+    hits_eff = [nai_effeciency(eV)*counts for eV, counts in zip(keV, hits)]
+
+    # compare template pre/post efficiency 
+    titles = ['NNDC gamma-ray table', 'table scaled for efficiency']
+    outfile = os.path.join(args.outdir, 'compare_efficiency.pdf')
+    show_spectra(keV, [hits, hits_eff], rn, titles, args.min_keV, args.max_keV, outfile)
+
+    # apply Gaussian broadening
+    hits_broad = np.array(data_smooth(keV, hits, **config['SMOOTH_PARAMS']))
+    hits_broad /= np.sqrt(np.sum(hits_broad**2))
+
+    # compare template pre/post efficiency 
+    titles = ['table scaled for efficiency', 'with Gaussian broadening']
+    outfile = os.path.join(args.outdir, 'compare_eff_broad.pdf')
+    show_spectra(keV, [hits_eff, hits_broad], rn, titles, args.min_keV, args.max_keV, outfile)
+
+    print(f'Script completed in {time.time()-start:.2f} secs')
+
+    return 0
+
+if __name__ == '__main__':
+    args = parse_args()
+    sys.exit(main(args))
